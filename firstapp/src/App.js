b179a29a2bc2a542751498b6b27accf45d2b0b7f
Scott Bui <scottngocbui@gmail.com>
2022-10-18 17:07:22 -0400

    Move heading function from Apps.js to Headings.js

--- firstapp/src/App.js
+++ firstapp/src/App.js
@@ -1,13 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
+import './components/Heading'
 
-function Heading() {
-  return <h1>This is an h1 heading</h1>;
-}
 function App() { 
   return ( 
     <div className="App"> 
-      This is the starting code for "Your first component" ungraded lab 
       <Heading /> 
     </div> 
   ); 
