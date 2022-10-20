866998e14b7e9730f8e0e79ea1c113dd64429d8b
Scott Bui <scottngocbui@gmail.com>
2022-10-20 15:53:28 -0400

    Changes to learn about props and children

--- apply-learning/src/App.js
+++ apply-learning/src/App.js
@@ -1,12 +1,15 @@
-import Heading from "./components/Heading";
+import "./App.css";
+import Card from "./components/Card.js"
 
 function App() {
-    return (
-        <div className="App">
-            <Heading firstName="Pauline" />
-            <Heading firstName="Scott" />
-        </div>
-    );
-};
+  return (
+    <div className="App">
+        <h1>Task: Add three Card elements</h1>
+        <Card h2="First card's h2" h3="First card's h3" />
+        <Card h2="Second card's h2" h3="Second card's h3"/>;
+        <Card h2="Third card's h2" h3="Third cards's h3"/>
+    </div>
+  );
+}
 
 export default App;
--- apply-learning/src/components/Nav.js
+++ apply-learning/src/components/Nav.js
@@ -1,14 +1,9 @@
-function Nav() {
+const Nav = (props) => {
     return (
-        <nav className="main-nav">
-            <ul>
-                <li>Home</li>
-                <li>Articles</li>
-                <li>About</li>
-                <li>Contact</li>
-            </ul>
-        </nav>
-    );
+        <ul>
+            <li>{props.first}</li>
+        </ul>
+    )
 };
 
 export default Nav;
