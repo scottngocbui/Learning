7d9cd12819d30cc685f96dcf950acbb873533fcd
Scott Bui <scottngocbui@gmail.com>
2022-10-20 12:49:09 -0400

    Changed App.js and Heading.js to test React props

--- apply-learning/src/components/Heading.js
+++ apply-learning/src/components/Heading.js
@@ -1,5 +1,7 @@
-function Heading() {
-    return <h1>This is an h1 heading</h1>;
-  }
+function Heading(props) {
+  return (
+      <h1>Hello, {props.firstName}</h1>
+  )
+}
 
-  export default Heading;
+export default Heading;
