858583f512c7e68c9e22a3de09a8fb4ccd5b4d18
Scott Bui <scottngocbui@gmail.com>
2022-10-21 16:02:27 -0400

    Changed app.js for event handling

--- apply-learning/src/App.js
+++ apply-learning/src/App.js
@@ -1,11 +1,18 @@
-import Btn from "./Btn.js"
-import "./App.css";
-import Card from "./components/Card.js"
-import ModeToggler from "./ModeToggler.js";
-
 function App() {
+  function handleClick() {
+    let randomNum = Math.floor(Math.random() * 3) + 1
+    console.log(randomNum)
+    let userInput = prompt('type a number');
+    alert(`Computer number: ${randomNum}, Your guess: ${userInput}`)
+}
+
   return (
-    <ModeToggler />
+    <div>
+      <h1>Task: Add a button and handle a click event</h1>
+      <button onClick={handleClick}>
+        Guess the number between 1 and 3.
+      </button>
+    </div>
   );
 }
 
