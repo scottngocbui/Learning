157d74645b67f7c68003a88ebbe5cc4d646a128d
Scott Bui <scottngocbui@gmail.com>
2022-10-21 15:45:12 -0400

    Finished ModeToggle learning: User Events

--- apply-learning/src/ModeToggler.js
+++ apply-learning/src/ModeToggler.js
@@ -1,14 +1,27 @@
 function ModeToggler() {
 
-    const darkModeOn = false;
+    let darkModeOn = false;
     const darkMode = <h1>Dark Mode is On</h1>
     const lightMode = <h1>Light Mode is On</h1>
 
+    function handleClick() {
+        darkModeOn = !darkModeOn;
+        if (darkModeOn == true){
+            console.log("Dark mode is on")
+        } else {
+            console.log("Light mode is on")
+        }
+    }
+
     return (
-        <div>
+       <div>
             {darkModeOn ? darkMode : lightMode}
+            <button onClick={handleClick}>
+                Click me
+            </button>
         </div>
     )
 }
+
 
 export default ModeToggler;
