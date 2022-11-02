10ab48efc4fa5d0f14c12f5834edaf4669ab8e36
Scott Bui <scottngocbui@gmail.com>
2022-11-02 14:45:50 -0400

    Starting Portfolio Website

---
+++ my-portfolio/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import Nav from './components/Nav.js';
+
+function App() {
+  return (
+    <div>
+      <Nav />
+    </div>
+  );
+}
+
+export default App;
---
+++ my-portfolio/src/components/Nav.js
@@ -0,0 +1,16 @@
+function Nav() {
+    return (
+        <header>
+            <nav className="main-nav">
+                <ul>
+                    <li>Home</li>
+                    <li>Articles</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                </ul>
+            </nav>
+        </header>
+    );
+};
+
+export default Nav;
